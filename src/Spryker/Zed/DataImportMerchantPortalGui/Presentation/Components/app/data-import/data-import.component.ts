2e0a391cffc3131403c3709b107f3858420404d5
Oleksii Bilan <alexey.belan@spryker.com>
2026-01-14 13:55:29 +0100

    CC-36826 Update Angular 20 (#197)
    CC-36826 Update Angular to v20

--- src/Spryker/Zed/DataImportMerchantPortalGui/Presentation/Components/app/data-import/data-import.component.ts
+++ src/Spryker/Zed/DataImportMerchantPortalGui/Presentation/Components/app/data-import/data-import.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
+    standalone: false,
     selector: 'mp-data-import',
     templateUrl: './data-import.component.html',
     styleUrls: ['./data-import.component.less'],
